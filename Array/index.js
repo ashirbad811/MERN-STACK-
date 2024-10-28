1966ed1cbadbd706deb6b5c1e742406263a13f71
Ashirbad <ashirbadmishra09@gmail.com>
2024-10-28 13:42:24 +0530

    commit

---
+++ Array/index.js
@@ -0,0 +1,29 @@
+// let arr = [(12),("ashirbad"),1.334]
+// let m = arr[2];
+// console.log( m);
+
+// let newarr = [] ;
+// console.log(newarr);
+// console.log(typeof(newarr));
+
+// let userarry = ["pablo",60,"criminal"]
+// console.log(userarry);
+
+// declaration
+// let newnumber = new Array (1,2,3,5,6);
+// console.log(newnumber);
+// console.log( newnumber[newnumber.length -1]);
+
+// let cities = ["bbsr","cutttack","bhadrak"]
+// cities.push("dhenkanal")
+// console.log(cities);
+// cities.pop()
+// console.log(cities);
+// cities.unshift("bhuban","kamakhya")
+// console.log(cities);
+// cities.shift()
+// console.log(cities);
+
+
+
+
---
+++ Array/readme.md
@@ -0,0 +1,22 @@
+array -: array is  collection of elements 
+why we use array
+single variable name 
+- structured way
+- easy to maintain
+- code reeadiablity
+
+Array declaration ->
+let newnumber = new Array (1,2,3,5,6)
+
+push() method 
+element will be added in last
+
+pop() method 
+element will be remove  from last
+
+array.shift()
+element will be deleted from first
+
+array.unshift()
+element will be add from first
+we can add more than one item in this method
